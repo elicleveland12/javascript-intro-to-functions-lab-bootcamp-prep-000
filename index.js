26bf38a77006a988dbba0142be1f95098f125601
Eli Cleveland <elicleveland12@gmail.com>
2018-01-12 17:58:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function shout(string) {
+  console.log(`{string}`).toUpperCase()
   return string.toUpperCase()
 }
 function whisper(string) {
+  console.log(`{string}`).toLowerCase()
   return string.toLowerCase()
 }
