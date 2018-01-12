41f1191ba5b679343baeedc9b8a7e0f310764f92
Eli Cleveland <elicleveland12@gmail.com>
2018-01-12 18:29:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function logShout(string) {
 function logWhisper(string){
   console.log('hello')
 }
-var uppercase = "HELLO!"
+var uppercase = "HELLO"
 uppercase.toUpperCase() === uppercase
-var lowercase = 'hello!'
+var lowercase = 'hello'
 lowercase.toLowerCase() === lowercase 
 function sayHiToGrandma(string) {
-  if (string = lowercase) {
+  if (string === lowercase) {
     return 'I can\'t hear you!'
-  } else if (string = uppercase) {
+  } else if (string === uppercase) {
     return 'YES INDEED!'
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
